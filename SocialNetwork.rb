c21bddd7b4b2c21745ca290fd9726b00ac442041
V. Stoyanov <devphp.web@gmail.com>
2016-01-07 20:30:30 +0200

    Created the PandaSocialNetwork class with its barebones.

---
+++ SocialNetwork.rb
@@ -0,0 +1,37 @@
+class PandaSocialNetwork
+  def initialize
+    @network = []
+  end
+
+  def add_panda(panda)
+
+  end
+
+  def has_panda(panda)
+
+  end
+
+  def make_friends(panda1, panda2)
+
+  end
+
+  def are_friends(panda1, panda2)
+
+  end
+
+  def friends_of(panda)
+
+  end
+
+  def connection_level(panda1, panda2)
+
+  end
+
+  def are_connected(panda1, panda2)
+
+  end
+
+  def how_many_gender_in_network(level, panda, gender)
+
+  end
+end
